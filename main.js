31cf498d98ec7fa142b224e923f74879862334b7
jeel-wb <jilsuthar609@gmail.com>
2026-03-27 16:26:06 +0530

    main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed('.text', {
-    strings: ["Front-end Developer", 'Web Developer'],
-    typeSpeed: 100,
-    backspeed: 100,
-    backDelay: 1000,
-    loop: true, // Default value
-});
